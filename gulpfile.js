959f5ef4346a850d0adc74a3a1f75d744516b496
marcoasb96 <62141980+marcoasb96@users.noreply.github.com>
2020-05-01 18:32:50 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sync = require('gulp-npm-script-sync');
+var cssnano = require('gulp-cssnano');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+gulp.task('sass', function(){
+    return gulp.src('app/style.scss')
+        .pipe(sass())
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('js', function(){
+    return gulp.src(['app/js/plugins/*.js', 'app/js/*.js'])
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('app/*.scss',  gulp.series('sass'));
+    gulp.watch('app/js/**/*.js', gulp.series('js'));
+});
+
+gulp.task('default', gulp.series('sass', 'js', 'watch'));
+
+sync(gulp);
